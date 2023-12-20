ecd842027d6bbb6a56c1b92fa52b306e4fd6dc3c
estebanmasaya <estebanmasaya@gmail.com>
2023-12-20 10:25:44 +0100

    Initial Commit

---
+++ BeatMotion/BeatMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatMotionApp.swift
+//  BeatMotion
+//
+//  Created by Esteban Masaya on 2023-12-20.
+//
+
+import SwiftUI
+
+@main
+struct BeatMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
